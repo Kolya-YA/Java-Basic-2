0ffe524b33230b2ea76f513f27cdbba1875263ad
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2022-08-18 10:35:09 +0200

    rename classes in package Lottery

--- src/main/java/com/telran/berlin/homeworks/lottery/StartLottery.java
+++ src/main/java/com/telran/berlin/homeworks/lottery/StartLottery.java
@@ -1,11 +1,12 @@
 package com.telran.berlin.homeworks.lottery;
 
-public class testLottery {
+public class StartLottery {
     public static void main(String[] args) {
+
         Lottery newL01 = new Lottery();
 
         newL01.setUserNumbers();
-        newL01.setWinNumbers(10, 20, 30, 40, 49);
+        newL01.setWinNumbers();
         System.out.println(newL01.getResult());
     }
 }
--- src/main/java/com/telran/berlin/homeworks/lottery/TestLottery.java
+++ src/main/java/com/telran/berlin/homeworks/lottery/TestLottery.java
@@ -1,12 +1,11 @@
 package com.telran.berlin.homeworks.lottery;
 
-public class startLottery {
+public class TestLottery {
     public static void main(String[] args) {
-
         Lottery newL01 = new Lottery();
 
         newL01.setUserNumbers();
-        newL01.setWinNumbers();
+        newL01.setWinNumbers(10, 20, 30, 40, 49);
         System.out.println(newL01.getResult());
     }
 }
